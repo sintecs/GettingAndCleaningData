34481b79ac972687e7fdada77722e6ac5fd0790d
Dave Bucholtz <dave@bucholtz.org>
2015-07-20 23:59:57 -0400

    Pass 3
    Steps 1 - 5 done, just need to fix some column names and test

--- run_analysis.R
+++ run_analysis.R
@@ -28,7 +28,7 @@ test.y          <- read.table(unz(temp, fileList[3]))
 train.subjects  <- read.table(unz(temp, fileList[4]))
 train.x         <- read.table(unz(temp, fileList[5]))
 train.y         <- read.table(unz(temp, fileList[6]))
-acivities       <- read.table(unz(temp, fileList[7]))
+activities      <- read.table(unz(temp, fileList[7]))
 features        <- read.table(unz(temp, fileList[8]))
 unlink(temp)
 
@@ -37,54 +37,46 @@ subjects              <- rbind(test.subjects, train.subjects)
 subjects.x            <- rbind(test.x, train.x)
 subjects.y            <- rbind(test.y, train.y)
 
-## Rename Columns in subjects, subjects.x and subjects.y
-colnames(subjects)    <- "Subject"
-colnames(subjects.y)  <- "Activity"
-colnames(subjects.x) <- features[, 2]
+## Rename Columns in subjects, subjects.x, subjects.y and activities
+colnames(subjects)    <- 'Subject'
+colnames(subjects.y)  <- 'ActivityID'
+colnames(subjects.x)  <- features[, 2]
+colnames(activities)  <- c('ActivityID', 'Activity')
 
 ## Loop through columns in features and keep only the columns we want
 ## Rename columns based on features.txt column names
-
-x <- 0
-trim.subjects.x <- data.frame()
+x <- 1
+y <- 1
+trim.subjects.x <- data.frame(rep(NA, length(subjects.x[, 1])))
 for (i in features[, 2])
 {
-   if (length(grep('mean()', i) > 0))
+   if (   length(grep('mean\\(\\)', i, ignore.case = TRUE) > 0) 
+       || length(grep('std\\(\\)', i, ignore.case = TRUE) > 0)
+      )
    {
-       ##trim.subjects.x <- cbind(trim.subjects.x, subjects.x[, x])
-       if (length(trim.subjects.x) == 0)
-       {
-           trim.subjects.x <- data.frame(subjects.x[, x])
-           print("make new dataframe mean")
-       }
-       else
-       {
-           trim.subjects.x[[i]] <- subjects.x[, x]
-           print("add to dataframe mean")
-       }
+       trim.subjects.x[y] <- subjects.x[, x]
+       colnames(trim.subjects.x)[y] <- colnames(subjects.x)[x]
+       y <- y + 1
    }
-   else if (length(grep('std()', i) > 0))
-   {
-       ##trim.subjects.x <- cbind(trim.subjects.x, subjects.x[, x])
-       if (length(trim.subjects.x) == 0)
-       {
-           trim.subjects.x <- data.frame(subjects.x[, x])
-           print("make new dataframe std")
-       }
-       else
-       {
-           trim.subjects.x[[i]] <- subjects.x[, x]
-           print("add to dataframe std")
-       }
-   }
-
    x <- x + 1
 }
 
+## Merge subjects.y and activities by ActivityID, make sure to not sort the data
+## or we will have the wrong activities with the wrong subjects!!!  
+## To do this we add an id column temporarily
+subjects.y$id         <- 1:nrow(subjects.y)
+tidy.subjects.y       <- merge(subjects.y, activities, by = 'ActivityID', sort = FALSE)
+tidy.subjects.y       <- tidy.subjects.y[order(tidy.subjects.y$id), ]
+tidy.subjects.y       <- data.frame(tidy.subjects.y[, 3])
+colnames(tidy.subjects.y)[1] <- 'Activity'
 
 ## Make one big data frame
-subjects.full         <- cbind(subjects, subjects.y, subjects.x)
+subjects.full         <- cbind(subjects, tidy.subjects.y, trim.subjects.x)
 
-
-
+## Get the average of each datapoint for each subject and activity pair
+grouping              <- list(
+                               Subject = subjects.full$Subject
+                              ,Activity = subjects.full$Activity
+                             )
+tidy.full             <- aggregate(subjects.full[, 3:68], grouping, mean, na.rm = TRUE)
 
